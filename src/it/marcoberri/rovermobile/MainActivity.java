e4e6ac8b0dba16946d59b9c8e3f91851a0b0c58e
Marco Berri <marcoberri@gmail.com>
2014-01-29 22:25:06 +0100

    start

--- src/it/marcoberri/rovermobile/MainActivity.java
+++ src/it/marcoberri/rovermobile/MainActivity.java
@@ -2,7 +2,8 @@ package it.marcoberri.rovermobile;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
@@ -10,13 +11,12 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
 
 }
