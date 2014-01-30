db5ee943cd0e272e8baf47ac2db72c9f5a962d97
marcoberri <marcoberri@gmail.com>
2014-01-30 10:19:53 +0100

    add first image

--- src/it/marcoberri/rovermobile/MainActivity.java
+++ src/it/marcoberri/rovermobile/MainActivity.java
@@ -10,12 +10,9 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		//Remove title bar
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+		setContentView(R.layout.activity_main);
 	}
 
 
