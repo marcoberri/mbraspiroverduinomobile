1bf3c910896343ba5277a2905c44ae1e27c51b7d
marcoberri <marcoberri@gmail.com>
2014-01-31 15:39:29 +0100

    try service

--- src/it/marcoberri/rovermobile/MainActivity.java
+++ src/it/marcoberri/rovermobile/MainActivity.java
@@ -1,11 +1,10 @@
 package it.marcoberri.rovermobile;
 
-import java.net.MalformedURLException;
-
-import io.socket.SocketIO;
-import it.marcoberri.rovermobile.helper.SocketHelper;
+import it.marcoberri.rovermobile.helper.SocketIOService;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -14,7 +13,7 @@ import android.view.WindowManager;
 public class MainActivity extends Activity {
 
 	
-	SocketHelper socket;
+	private Intent socketIntenet;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,22 +21,37 @@ public class MainActivity extends Activity {
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
-		socket = new SocketHelper();
+		
+
+		
+		
+		
+		
+		
+		/*
+		
+		socket = new SocketIOService();
 		try {
 			socket.connect("http://172.18.205.88:3001/");
+			socket.run();
 		} catch (MalformedURLException e) {
 			Log.e("SOCKET",e.getMessage());
-		}
+		} catch (Exception e) {
+			Log.e("SOCKET",e.getMessage());
+		} */
 	}
 
 	
 	public void callStop(View v){
-		try {
-			socket.call("/move/left");
-		} catch (Exception e) {
-			Log.e("SOCKET",e.getMessage());
-		}
-	        
+		
+		socketIntenet = new Intent(this, SocketIOService.class); 
+		 startService(socketIntenet);
+/*
+		Intent mServiceIntent = new Intent(this, SocketIOService.class);
+		mServiceIntent.setData(Uri.parse("http://172.18.205.88:3001/"));
+		startService(mServiceIntent);
+*/
+		Log.d("SOCKET", "START");
 	}
 	
 }
